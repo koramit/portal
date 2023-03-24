c898165ec771e8023160c7dcc9f74397f30e9ca9
koramit <koramit.p@gmail.com>
2023-03-24 18:08:50 +0700

    typo

--- config/siad.php
+++ config/siad.php
@@ -11,10 +11,10 @@ return [
     |
     */
 
-    'check_user_url' => env('SIAD_CHECK_USER__URL'),
+    'check_user_url' => env('SIAD_CHECK_USER_URL'),
     'check_user_app_name' => env('SIAD_CHECK_USER_APP_NAME'),
     'check_user_api_key' => env('SIAD_CHECK_USER_API_KEY'),
-    'auth_user_url' => env('SIAD_AUTH_USER__URL'),
+    'auth_user_url' => env('SIAD_AUTH_USER_URL'),
     'auth_user_app_name' => env('SIAD_AUTH_USER_APP_NAME'),
     'auth_user_api_key' => env('SIAD_AUTH_USER_API_KEY'),
     'user_status_url' => env('SIAD_USER_STATUS_URL'),
