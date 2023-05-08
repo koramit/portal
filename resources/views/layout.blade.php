69718a4c27d23ee7586a6217ab76b33c930ed608
koramit <koramit.p@gmail.com>
2023-05-08 10:54:14 +0700

    add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('partials.favicon')
     <title>{{ $title }}</title>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@400;600;700&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css'])
