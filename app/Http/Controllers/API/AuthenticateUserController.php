3685dcacbbe549538aa5a2d49c7cbd826ab9d7e5
nongsansanee <nong_com@hotmail.com>
2023-06-12 15:30:18 +0700

    edit pattern validate username

--- app/Http/Controllers/API/AuthenticateUserController.php
+++ app/Http/Controllers/API/AuthenticateUserController.php
@@ -14,7 +14,7 @@ class AuthenticateUserController extends Controller
     public function __invoke(Request $request, UserAPI $api)
     {
         $validated = $request->validate([
-            'login' => ['required', 'regex:/^[a-zA-Z]{1,24}\.[a-zA-Z]{3}$/'],
+            'login' => ['required', 'regex:/^[a-zA-Z]{1,24}\....$/'],
             'password' => ['required', 'string'],
         ]);
 
