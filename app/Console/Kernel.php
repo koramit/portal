63f805bb42fc73eba1066d7d8f77ef76b33b49e9
koramit <koramit.p@gmail.com>
2023-08-12 13:03:38 +0700

    add token expired reminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('token:expired-reminder')->timezone('Asia/Bangkok')->at('08:00');
     }
 
     /**
