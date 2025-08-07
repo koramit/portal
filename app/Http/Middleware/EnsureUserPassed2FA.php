4b026d81b740ec4aa49771e97de46e6031a0a5ce
koramit <koramit.p@gmail.com>
2025-08-07 14:11:37 +0700

    add middleware

--- app/Http/Middleware/EnsureUserPassed2FA.php
+++ app/Http/Middleware/EnsureUserPassed2FA.php
@@ -4,11 +4,15 @@ namespace App\Http\Middleware;
 
 use App\Services\TwoFactorService;
 use Closure;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserPassed2FA
 {
+    /**
+     * @throws ConnectionException
+     */
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->user()->notifiable && session('2fa_passed') !== true) {
